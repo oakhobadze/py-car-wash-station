e71ef24402b80dde1b453f55dd1a9026589e5cf9
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2024-11-19 17:38:11 +0100

    fix problems about flake8 in py-car-wash-station

--- app/main.py
+++ app/main.py
@@ -1,22 +1,22 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
-        self.comfort_class = comfort_class
-        self.clean_mark = clean_mark
+    def __init__(self, comfort: int, clean: int, brand: str) -> None:
+        self.comfort_class = comfort
+        self.clean_mark = clean
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int, clean_power: int, average_rating: float,
-                 count_of_ratings: int) -> None:
-        self.distance_from_city_center = distance_from_city_center
-        self.clean_power = clean_power
-        self.average_rating = average_rating
-        self.count_of_ratings = count_of_ratings
+    def __init__(self, dist: int, power: int, mark: float, count: int) -> None:
+        self.distance_from_city_center = dist
+        self.clean_power = power
+        self.average_rating = mark
+        self.count_of_ratings = count
 
     def calculate_washing_price(self, car: Car) -> float:
         if self.distance_from_city_center != 0:
-            return round(car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating /
-                         self.distance_from_city_center, 1)
+            first = car.comfort_class * (self.clean_power - car.clean_mark)
+            second = self.distance_from_city_center
+            return round(first * self.average_rating / second, 1)
         else:
             return 0.0
 
@@ -34,6 +34,8 @@ class CarWashStation:
             car.clean_mark = self.clean_power
 
     def rate_service(self, mark: int) -> None:
-        new_mark = round((self.average_rating * self.count_of_ratings + mark) / (self.count_of_ratings + 1), 1)
+        first = (self.average_rating * self.count_of_ratings + mark)
+        second = (self.count_of_ratings + 1)
+        new_mark = round(first / second, 1)
         self.average_rating = new_mark
         self.count_of_ratings += 1
